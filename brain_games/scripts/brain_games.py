432656f0851a71b0fddfa20054bbf1fc64edab4b
Pavel Grigorev <pavel0408@gmail.com>
2022-09-26 22:55:24 +0300

    step3: add prompt and name asking

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+import prompt
+
 
 def greet():
     print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
 
 
 def main():
